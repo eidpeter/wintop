ea1a32da88dd8a096d0ebecdd2e028c56ea08b4b
Peter Eid <peter.m.eid@outlook.com>
2022-01-10 20:43:14 +0200

    Add initial project files

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace wintop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
